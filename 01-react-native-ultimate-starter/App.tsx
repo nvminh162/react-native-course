9a0e074d1ac067445797ed219613d2b0c2e93280
nvminh162 <nvminh162@gmail.com>
2025-06-02 18:53:30 +0700

    how to render array.map() item in react native?

--- 01-react-native-ultimate-starter/App.tsx
+++ 01-react-native-ultimate-starter/App.tsx
@@ -2,31 +2,29 @@ import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  //String
-  const [name, setName] = useState<string>("");
+  const [nameTodo, setNameTodo] = useState<string>("");
 
-  //number
-  const [age, setAge] = useState<number>(0);
-
-  //null, undefined, boolean
-  const test = null;
-
-  //object, array
-  const [person, setPerson] = useState<{
-    name: string;
-    age: number;
-  }>({
-    name: "nvminh162",
-    age: 22,
-  });
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Learn react native 1" },
+    { id: 2, title: "Learn react native 2" },
+    { id: 3, title: "Learn react native 3" },
+    { id: 4, title: "Learn react native 4" },
+    { id: 5, title: "Learn react native 5" },
+    { id: 6, title: "Learn react native 6" },
+    { id: 7, title: "Learn react native 7" },
+    { id: 8, title: "Learn react native 8" },
+    { id: 9, title: "Learn react native 9" },
+    { id: 10, title: "Learn react native 10" },
+    { id: 11, title: "Learn react native 11" },
+  ]);
 
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.text}>Input</Text>
         <TextInput
-          value={name}
-          onChangeText={(v) => setName(v)}
+          value={nameTodo}
+          onChangeText={(v) => setNameTodo(v)}
           autoCapitalize="none"
           style={styles.input}
           // maxLength={2}
@@ -35,13 +33,17 @@ export default function App() {
           autoCorrect={false}
         />
       </View>
-      <Button title="Add New" />
+      <Button title="Add New" color={"red"} onPress={() => alert(nameTodo)} />
       <View>
         <Text style={styles.text}>
-          from
-          <Text style={styles.nvminh162}> {name} </Text>
-          with love
+          Typing ...
+          <Text style={styles.todoName}> {nameTodo} </Text>
         </Text>
+      </View>
+      <View>
+        {todoList.map((todo) => (
+          <Text key={todo.id} style={styles.todo}>{todo.title}</Text>
+        ))}
       </View>
     </View>
   );
@@ -51,18 +53,26 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
     color: "red",
+    marginVertical: 20,
   },
-  nvminh162: {
+  todoName: {
     color: "green",
+  },
+  todo: {
+    fontSize: 30,
+    backgroundColor: "pink",
+    marginBottom: 20,
+    padding: 10,
   },
   input: {
     borderColor: "black",
     borderWidth: 1,
     borderRadius: 20,
+    marginVertical: 10,
   },
   container: {
     flex: 1,
-    padding: 20,
+    marginTop: 50,
     backgroundColor: "#fff",
     // alignItems: "center",
     // justifyContent: "center",
