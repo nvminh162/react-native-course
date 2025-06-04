6d2b9569e64ed3ba159940b471c9a758ff4ebe2f
nvminh162 <nvminh162@gmail.com>
2025-06-04 18:30:46 +0700

    how to Nesting between <Stack/>(Parent) and <Tabs/>(Child) in expo router .....:!:.......

--- react-expo-router/src/app/_layout.tsx
+++ react-expo-router/src/app/_layout.tsx
@@ -2,23 +2,8 @@ import { Stack } from "expo-router";
 
 const RootLayout = () => {
   return (
-    /* <View style={{ padding: 50 }}>
-      <Text>Header</Text>
-      <Slot />
-      <Text>Footer</Text>
-    </View> */
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#f4511e",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen name="index" options={{ headerTitle: "Trang chủ" }} />
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerTitle: "Trang chủ" }} />
       <Stack.Screen
         name="product/index"
         options={{ headerTitle: "Sản phẩm" }}
